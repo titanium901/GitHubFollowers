cfc02458dbe271e085e5960c77b7c2a70937f6b4
titanium901 <nemes901@hotmail.com>
2020-02-05 20:26:43 +0300

    First commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Iury Popov on 05.02.2020.
+//  Copyright © 2020 Iurii Popov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
