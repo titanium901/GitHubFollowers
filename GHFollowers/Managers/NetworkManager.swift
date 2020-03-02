08ccdfff164e18032fc13eb0a9cd8c25b220b64c
titanium901 <nemes901@hotmail.com>
2020-03-02 21:15:23 +0300

    First network call

---
+++ GHFollowers/Managers/NetworkManager.swift
@@ -0,0 +1,54 @@
+//
+//  NetworkManager.swift
+//  GHFollowers
+//
+//  Created by Iury Popov on 11.02.2020.
+//  Copyright © 2020 Iurii Popov. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    let baseURL = "https://api.github.com/users/"
+    
+    private init() {}
+    
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void) {
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completed(nil, .invalidUsername)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let _ = error {
+                completed(nil, .unableToComplete)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(nil, .invalidResponse)
+                return
+            }
+            
+            guard let data = data else {
+                completed(nil, .invalidData)
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completed(followers, nil)
+            } catch {
+                completed(nil, .invalidData)
+            }
+        }
+        
+        task.resume()
+    }
+}
