e22a43b683fd6a7ad2668a42c1538e8fd1f54e2f
titanium901 <nemes901@hotmail.com>
2020-03-04 21:02:01 +0300

    SetUp collectionView

---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Iury Popov on 04.03.2020.
+//  Copyright © 2020 Iurii Popov. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+      override init(frame: CGRect) {
+          super.init(frame: frame)
+          configure()
+      }
+      
+      required init?(coder: NSCoder) {
+          fatalError("init(coder:) has not been implemented")
+      }
+      
+      private func configure() {
+          layer.cornerRadius  = 10
+          clipsToBounds       = true
+          image               = placeholderImage
+          translatesAutoresizingMaskIntoConstraints = false
+      }
+}
